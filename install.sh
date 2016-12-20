6a3b98bed73b99ebdaa4e52a9c8142da44dc3bbc
andrew jones <andrew@andrewjones.id.au>
2016-12-21 10:18:14 +1100

    add super basic install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf ${DIR}/.zshrc ~/.zshrc
+ln -sf ${DIR}/.spacemacs ~/.spacemacs
+ln -sf ${DIR}/.gitconfig ~/.gitconfig
+ln -sf ${DIR}/.gitignore ~/.gitignore
+ln -sf ${DIR}/.vim ~/.vim
+ln -sf ${DIR}/.vimrc ~/.vimrc
+ln -sf ${DIR}/.ackrc ~/.ackrc
+ln -sf ${DIR}/.gemrc ~/.gemrc
+ln -sf ${DIR}/.rspec ~/.rspec
