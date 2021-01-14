f184a0db039a62ac84791fd2adb26fee91737307
Andrew Jones <andrew@andrewjones.id.au>
2021-01-15 10:27:45 +1100

    Set up for Typescript development

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -75,3 +75,48 @@
 (rvm-use-default)
 
 (setq projectile-indexing-method 'native)
+
+(require 'flycheck)
+(defun tide-setup-hook ()
+    (tide-setup)
+    (eldoc-mode)
+    (run-import-js)
+    (tide-hl-identifier-mode +1)
+    (setq web-mode-enable-auto-quoting nil)
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-attr-indent-offset 2)
+    (setq web-mode-attr-value-indent-offset 2)
+    (set (make-local-variable 'company-backends)
+         '((company-tide company-files :with company-yasnippet)
+           (company-dabbrev-code company-dabbrev))))
+
+;; hooks
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+
+;; use rjsx-mode for .js* files except json and use tide with rjsx
+(add-to-list 'auto-mode-alist '("\\.js.*$" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+(add-hook 'rjsx-mode-hook 'tide-setup-hook)
+
+
+;; web-mode extra config
+(add-hook 'web-mode-hook 'tide-setup-hook
+          (lambda () (pcase (file-name-extension buffer-file-name)
+                  ("tsx" ('tide-setup-hook))
+                  (_ (my-web-mode-hook)))))
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+(add-hook 'web-mode-hook 'company-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook #'turn-on-smartparens-mode t)
+
+;; yasnippet
+(yas-global-mode 1)
+
+;; flycheck
+(global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; company-mode
+(global-company-mode)
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -50,3 +50,4 @@
 ;(unpin! t)
 
 (package! evil-colemak-basics)
+(package! prettier-js)
